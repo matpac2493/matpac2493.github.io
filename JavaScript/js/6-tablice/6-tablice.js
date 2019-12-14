70c85de384316a3ad99dcbfb249a87f23f2c8127
Mateusz Pachuta <mateuszpachuta93@gmail.com>
2019-12-14 14:10:18 +0100

    Stworzono folder 6-tablice

---
+++ JavaScript/js/6-tablice/6-tablice.js
@@ -0,0 +1,28 @@
+let imiona =['Krystian', 'Jan','Aneta']
+console.log(imiona)
+imiona.unshift('Karolina')
+console.log(imiona)
+imiona.shift('Antek')
+console.log(imiona)
+
+
+
+let janKowalskiObj = {
+    imie:'Jan',
+    nazwisko: 'Kowalski',
+    pensja:5000,
+    dzieci:0
+}
+
+let monikaKowalskaObj = {
+    pensja:5500,
+    nazwisko:'Kowalski',
+    imie: 'Monika',
+    dzieci:3,
+
+}
+
+conslole.log(monikaKowalskaObj.dzieci)
+conslole.log(janKowalskiObj.dzieci)
+
+
