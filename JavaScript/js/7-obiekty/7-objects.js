c32eec877acd17c753b0ab6418811a334b9e431c
Mateusz Pachuta <mateuszpachuta93@gmail.com>
2019-12-14 14:10:39 +0100

    Stworzono folder 7-obiekty

---
+++ JavaScript/js/7-obiekty/7-objects.js
@@ -0,0 +1,23 @@
+let person = {
+    name: 'Marcin',
+    height: 2,
+    waga: 150
+
+
+}
+
+let bmi = person.waga/Math.pow(person.height,2)
+console.log(bmi)
+
+let numer =Math.floor(Math.random()*500)
+console.log(numer)
+
+
+let raz =2
+let dwa = 3
+
+
+if(raz>0 && dwa>0) {
+    alert("brawo")
+}
+
