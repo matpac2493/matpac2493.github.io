45e9fffcf472f82fa9b001be5460ea346b6cf209
Mateusz Pachuta <mateuszpachuta93@gmail.com>
2019-12-14 14:09:42 +0100

    Stworzono folder 5-datatype

---
+++ JavaScript/js/5-datatype/5-datatype.js
@@ -0,0 +1,23 @@
+function wstawanie () {
+    alert(123)
+
+
+}
+
+// wywołanie funkcji 
+wstawanie ();
+
+function dodawanie(parametr1, parametr2) {
+let wynik = parametr1 + parametr2
+console.log(wynik)
+
+}
+
+dodawanie(100,200)
+
+dodawanie(5,4)
+
+dodawanie(10,11)
+
+
+
